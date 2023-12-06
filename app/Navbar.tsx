07ad2836d23d51f81db2f35e6a513ac3e6ee9f67
mehulambastha <mehul.213amb@gmail.com>
2023-12-06 22:59:19 +0530

    homepage setup

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,18 +1,22 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
 import {motion} from "framer-motion"
 
+
 const Navbar = () => {
   return (
-    <div>
-      <div className="fixed inset-x-0 mx-auto flex justify-around flex-row w-1/4 rounded-b-2xl p-2 text-center bg-zinc-800 border-r-2 border-b-2 border-l-2">   
+      <motion.div 
+        initial={{y: -100}}
+        animate={{y: 0}}
+        exit={{y: -100}}
+        transition={{duration: 0.8, delay: 2}}
+        className={"fixed z-50 inset-x-0 mx-auto flex justify-around flex-row w-1/4 rounded-b-2xl p-2 text-center bg-zinc-900 border-r-2 border-b-2 border-l-2"}>   
         <p>
         <Link href="/play/">Play</Link>
-
         </p>
         <Link href="/rules">Rules</Link>
-      </div>      
-    </div>
+      </motion.div>      
   )
 }
 export default Navbar
--- app/components/HeroComponent/HeroComponent.tsx
+++ app/components/HeroComponent/HeroComponent.tsx
@@ -11,19 +11,20 @@ const HeroComponent = () => {
       className={styles.hero}
     >
       <motion.main 
-        initial= {{opacity: 0, y: -100}}
-        animate= {{opacity: 1, y: -10}}
-        exit={{opacity: 0, y: 100}}
-        transition={{delay: 0.50, duration: 0.4}}
+        initial= {{opacity: 0, y: -500, scale: 0.5}}
+        animate= {{opacity: 1, y: -10, scale: 1}}
+        exit={{opacity: 0, y: 100, scale: 1}}
+        transition={{delay: 0.20, duration: 0.8}}
         className={"flex min-h-screen w-full flex-col items-center justify-between p-24"}
       >
         <h1 className={"text-center text-8xl"}>Abhedya</h1>
       </motion.main>      
-        <motion.h2
-          initial= {{opacity: 0, y: 300}}
-          animate = {{opacity: 1, y: -50}}
-          transition={{delay: 1.2, duration: 0.7}}
-          className="text-center">Find the Mystery within</motion.h2>
+      <motion.h2
+        initial= {{opacity: 0, y: 300}}
+        animate = {{opacity: 1, y: -50}}
+        transition={{delay: 1, duration: 0.5}}
+        className="text-center">Find the Mystery within
+      </motion.h2>
     </div>
     </AnimatePresence>
   )
