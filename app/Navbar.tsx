d4021307213a5197899a0934bb681f446a6ccd77
mehulambastha <mehul.213amb@gmail.com>
2023-12-06 23:40:18 +0530

    fixed typo

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -13,6 +13,7 @@ const Navbar = () => {
         transition={{duration: 0.8, delay: 2}}
         className={"fixed z-50 inset-x-0 mx-auto flex justify-around flex-row w-1/4 rounded-b-2xl p-2 text-center bg-zinc-900 border-r-2 border-b-2 border-l-2"}>   
         <p>
+          
         <Link href="/play/">Play</Link>
         </p>
         <Link href="/rules">Rules</Link>
