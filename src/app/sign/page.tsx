40ab48f9ef19846e54e66252e0c3c2b6843e63cc
Sourabh-awasthy <sourabhawasthy176022@gmail.com>
2024-03-20 20:16:40 +0530

    signin

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -16,14 +16,12 @@ export default function SignupFormDemo() {
     console.log("Form submitted");
   };
   return (
- <div className="bg-black antialiased bd-grid-white/[0.02]">
+ <div className=" antialiased bd-grid-white/[0.02]">
      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black mt-20">
       <h2 className="font-bold text-2xl text-neutral-800 dark:text-neutral-200">
-        Welcome to ABHEDYA 3.0
+      Sign Up to Abhedya2k24
       </h2>
-      <p className="text-neutral-600 text-xl max-w-sm mt-2 dark:text-neutral-300">
-        Sign up to start game 
-      </p>
+      
 
       <form className="my-8" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
@@ -37,21 +35,14 @@ export default function SignupFormDemo() {
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" placeholder="username" type="text" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
         </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Your twitter password</Label>
-          <Input
-            id="twitterpassword"
-            placeholder="••••••••"
-            type="twitterpassword"
-          />
-        </LabelInputContainer>
+        
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
