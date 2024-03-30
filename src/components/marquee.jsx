6d502fa1f910ab974c6df4ea96bc6559b67f75a8
mehulambastha <mehul.213amb@gmail.com>
2024-03-31 02:16:39 +0530

    apos fix

--- src/components/marquee.jsx
+++ src/components/marquee.jsx
@@ -6,8 +6,8 @@ const Marquee = () => {
   return (
     <FastMarquee speed={50} direction="left" className="bg-gray-800 py-2">
       <div className='text-md'>
-        <p>&nbsp;10 Teams, 4 prizes, minimum <span className='underline text-blue-300'>15K prize per event</span> - jeetne ki maths khud hi laga lo - <span className="font-bold text-blue-400">&nbsp;<a href='prody.istenith.com' target='_blank'>Prody24'</a></span>&nbsp;|&nbsp;
-        Minimal Effort, High reward  - competitions for all branches.&nbsp; <a className="underline text-green-500 font-bold" target='_blank' href="http://prody.istenith.com/events"> Explore events&rarr;</a></p>
+        <p>&nbsp;10 Teams, 4 prizes, minimum <span className='underline text-blue-300'>15K prize per event</span> - jeetne ki maths khud hi laga lo - <span className="font-bold text-blue-400">&nbsp;<a href='prody.istenith.com' target='_blank'>Prody24&apos;</a></span>&nbsp;|&nbsp;
+        Minimal Effort, High reward  - competitions for all branches.&nbsp; <a className="underline text-green-500 font-bold" target='_blank' href="http://prody.istenith.com/events">Explore events&rarr;</a></p>
       </div>       
       
     </FastMarquee>
